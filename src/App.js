f2f86ecb2214fb7897ff37043b75c00192a89ca1
San Nge <samngestep@gmail.com>
2020-12-12 00:25:26 -0800

    implementation done

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
 			if (loading) return;
 			if (observer.current) observer.current.disconnect();
 			observer.current = new IntersectionObserver((entries) => {
-				if (entries[0].isIntersecting) {
-					console.log("isVisible");
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber((prev) => prev + 1);
 				}
 			});
 
